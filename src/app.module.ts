20003ba8d3df2290da8529bc815075ffc0c845a7
mahdi <mahdibouaziz@hotmail.fr>
2022-01-20 16:52:59 +0100

    basic auth with username and password - passport local

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { ConfigModule } from '@nestjs/config';
       `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.sr2ks.mongodb.net/casadelpadel?retryWrites=true&w=majority`,
     ),
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+// import { UpdateUserDto } from './dto/update-user.dto';
 import { User, UserDocument } from './entities/user.entity';
 // import * as mongoose from 'mongoose';
 
@@ -19,8 +19,8 @@ export class UsersService {
     return await this.userModel.find();
   }
 
-  async findOne(id: string): Promise<User> {
-    return await this.userModel.findById(id);
+  async findOne(email: string): Promise<User> {
+    return await this.userModel.findOne({ email });
   }
 
   // update(id: string, updateUserDto: UpdateUserDto) {
